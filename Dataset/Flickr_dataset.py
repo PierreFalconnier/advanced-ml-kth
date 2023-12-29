4981102ccd275ad052c4e24841a58ad93119cea0
Falconnier <pierrefalco@hotmail.fr>
2023-12-29 13:19:33 +0100

    Flickr

---
+++ Dataset/Flickr_dataset.py
@@ -0,0 +1,25 @@
+# SCRIPT AUTHOR: PIERRE FALCONNIER
+
+if __name__ == "__main__":
+    from pathlib import Path
+    from torch_geometric.datasets import Flickr
+    from torch_geometric.loader import NeighborLoader
+
+    # Dataset
+    DATA_DIR = Path(__file__).parents[1] / "Data" / "Flickr"
+    dataset = Flickr(root=DATA_DIR)[0]
+    batch_size = 512
+    num_neighbors = [25, 10]
+
+    # NeighborLoader
+    loader = NeighborLoader(
+        dataset,
+        num_neighbors=num_neighbors,
+        batch_size=batch_size,
+        shuffle=True,
+        input_nodes=dataset.train_mask,
+        num_workers=6,
+    )
+
+    print(dataset)
+    print(loader)
