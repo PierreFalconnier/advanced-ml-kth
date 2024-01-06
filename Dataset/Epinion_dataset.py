d17dae241131d8bb8a2874b8287d807c21775b2f
Falconnier <pierrefalco@hotmail.fr>
2024-01-06 15:19:53 +0100

    added Epinion_dataset

---
+++ Dataset/Epinion_dataset.py
@@ -0,0 +1,122 @@
+import os
+import requests
+import gzip
+import shutil
+import torch
+import pandas as pd
+import networkx as nx
+from pathlib import Path
+from torch_geometric.data import Data, Dataset
+from torch_geometric.utils import degree, to_networkx
+
+
+class EpinionsDataset(Dataset):
+    def __init__(
+        self, root, feature_mode="mean_degree", transform=None, pre_transform=None
+    ):
+        self.url = "https://snap.stanford.edu/data/soc-Epinions1.txt.gz"
+        # self.num_nodes = 75879  # As per the dataset information
+        self.feature_mode = feature_mode
+        super(EpinionsDataset, self).__init__(root, transform, pre_transform)
+
+    @property
+    def raw_file_names(self):
+        return ["soc-Epinions1.txt"]
+
+    @property
+    def processed_file_names(self):
+        return ["data.pt"]
+
+    def download(self):
+        gz_file_path = Path(self.raw_dir) / "soc-Epinions1.txt.gz"
+        txt_file_path = Path(self.raw_dir) / "soc-Epinions1.txt"
+        print("Downloading the dataset...")
+        response = requests.get(self.url, stream=True)
+        if response.status_code == 200:
+            with open(gz_file_path, "wb") as f:
+                f.write(response.content)
+            print("Dataset downloaded successfully.")
+
+            # Extract the .gz file
+            print("Extracting the dataset...")
+            with gzip.open(gz_file_path, "rb") as f_in:
+                with open(txt_file_path, "wb") as f_out:
+                    shutil.copyfileobj(f_in, f_out)
+            print("Dataset extracted successfully.")
+        else:
+            print(
+                "Failed to download the dataset. Please check the URL or your internet connection."
+            )
+
+    def process(self):
+        edge_index = read_edge_list(Path(self.raw_dir) / self.raw_file_names[0])
+        self.num_nodes = int(edge_index.max()) + 1
+
+        # Create a NetworkX graph and add all nodes
+        G = nx.Graph()
+        G.add_nodes_from(range(self.num_nodes))  # Explicitly add all nodes
+        G.add_edges_from(edge_index.t().numpy())
+
+        # Compute features for each node
+        if self.feature_mode == "mean_degree":
+            features = []
+            for node in range(self.num_nodes):
+                neighbors = list(G.neighbors(node))
+                if neighbors:
+                    neighbor_degrees = torch.tensor(
+                        [G.degree(neighbor) for neighbor in neighbors],
+                        dtype=torch.float,
+                    )
+                    feature = (
+                        torch.mean(neighbor_degrees).unsqueeze(0)
+                        if neighbor_degrees.numel() > 0
+                        else torch.tensor([0.0])
+                    )
+                else:  # nodes without neighbors
+                    feature = torch.tensor([0.0])
+                features.append(feature)
+
+            x = torch.cat(features, dim=0).unsqueeze(-1)
+
+        elif self.feature_mode == "degree":
+            deg = degree(edge_index[0], num_nodes=self.num_nodes)
+            x = deg.unsqueeze(
+                1
+            )  # Creating a feature matrix with degrees as the only feature
+
+        else:
+            print("Invalid feature mode")
+            raise ValueError()
+
+        data = Data(x=x, edge_index=edge_index)
+        if self.pre_transform is not None:
+            data = self.pre_transform(data)
+
+        torch.save(data, Path(self.processed_dir) / self.processed_file_names[0])
+
+    def len(self):
+        return 1
+
+    def get(self, idx):
+        data = torch.load(Path(self.processed_dir) / self.processed_file_names[0])
+        return data
+
+
+def read_edge_list(file_path):
+    edges = pd.read_csv(
+        file_path, sep="\t", comment="#", header=None, names=["source", "target"]
+    )
+    edge_index = torch.tensor(edges.values, dtype=torch.long).t().contiguous()
+    return edge_index
+
+
+# Usage example
+if __name__ == "__main__":
+    dataset_name = "Epinions"
+    root_path = Path(__file__).parents[1] / "Data" / dataset_name
+    dataset = EpinionsDataset(root=str(root_path), feature_mode="mean_degree")
+
+    # Access the data
+    data = dataset[0]
+    print(data.x)
+    print(data.x.shape)
---
+++ Run/main_graphsage_epinions.py
@@ -0,0 +1,37 @@
+# SCRIPT AUTHOR: PIERRE FALCONNIER
+
+if __name__ == "__main__":
+    # Imports
+    import sys
+    from pathlib import Path
+    import torch
+
+    torch.manual_seed(42)
+    # include the path of the dataset(s) and the model(s)
+    ROOT = Path(__file__).parents[1]
+    if str(ROOT) not in sys.path:
+        sys.path.append(str(ROOT))  # add ROOT to PATH
+
+    from Model.GraphSAGE import GraphSAGE
+    from Model.node_classification import node_classification_evaluation
+    from Dataset.Epinion_dataset import EpinionsDataset
+
+    # dataset
+    dataset_name = "Epinions"
+    DATA_DIR = ROOT / "Data" / dataset_name
+    dataset = EpinionsDataset(root=DATA_DIR)[0]
+
+    # GraphSAGE model
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = GraphSAGE(
+        in_channels=dataset.x.size(1), hidden_channels=1024, out_channels=128
+    ).to(device)
+
+    # fit
+    RESULT_PATH = ROOT / "Run" / "Results" / dataset_name
+    RESULT_PATH.mkdir(parents=True, exist_ok=True)
+    model.fit(dataset, num_epoch=10, batch_size=512, lr=0.0001, path=RESULT_PATH)
+    # model.load(RESULT_PATH / "graphSAGE_20240106_105728.pt")
+
+    # No node classification on this dataset because no labels
+    # Only link prediction. To be added...
